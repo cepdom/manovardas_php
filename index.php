cb02430cb71650f42119cba309ccddb3c6344a0c
Domantas Cepulis <cepdom@gmail.com>
2019-10-29 16:03:13 +0200

    Praktika su PHP2

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML <?php print 'ir PHP!' ?> !</title>
   </head>
 
 <body>
-<h1> <i> <b> Domantas </i> </b>   - HTML asas! </h1>
-<p> Moku kurti statinį turinį </p>
+<h1> <i> <b> Domantas </i> </b>  - HTML <?php print 'ir PHP' ?> asas! </h1>
+
+<p> Moku kurti dinaminį turinį.
+    Atidaryta: <?php print date('y/m/d h:m:s') ?> 
+
 </body>
 
 </html>
