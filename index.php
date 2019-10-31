30632e1791b28534714f102f2d7b5dea94e13238
Domantas Cepulis <cepdom@gmail.com>
2019-10-31 15:05:00 +0200

    variables

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-<html>
-    <head>
-       <meta charset="utf-8">
-       <title>PHP lydės ir <?php print date('d', strtotime('+1 day'));?></title>
-    </head>
-    <body>
-        <h1>
-            <b>Domantas</b> - HTML su manim buvo ir <?php print date('h', strtotime('-1 ')) . ' valandą!' ;?>
-        </h1>
-        <p>
-           <?php print date('y', strtotime('+1 year')); ?> ne už kalnų!
-        </p>
-    </body>
-</html>
+
+    <?php
+    $string = 'pvz';
+    $intteger = 0;
+    $boolean = true;
+    $nothing = null;
+    ?>
+
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title> </title>
+        </head>
+        <body>
+            <ul>
+                <li><?php print "$string";?>
+                    <li><?php print "$intteger";?>
+                        <li><?php print "$boolean";?>
+                            <li><?php print "$nothing";?>
+                    </body>
+                    </html>
